3a45bfa9c6935604814510d7a1b8f0b2e9e828f8
facundo <facundoacastello@gmail.com>
2020-03-06 21:00:11 -0300

    PIR}

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,30 @@
 #include "DHTSensor.h"
 #include "Buzzer.h"
 #include "Relay.h"
+#include "WiFi.h"
+#include "Ota.h"
 
 const uint16_t lightSensor = A0;
 const uint16_t DHT = D1;
 const uint16_t IrLed = D2;
+const uint16_t pirPin = D4;
 const uint16_t buzzer = D5;
 const uint16_t relay = 13; //D7
 
-uint32_t delayMS = 5000;
+uint32_t delayMS = 1000;
 unsigned long previousTimeGeneral = 0;
 
 void setup()
 {
   Serial.begin(9600);
-
+  setupBuzzer(buzzer, 800);
+  initWifi();
+  handleBuzzer(true);
+  setupOta();
+  pinMode(pirPin, INPUT);
   setupDHTSensor(DHT);
   setupLightSensor(lightSensor);
   setupIr(IrLed);
-  setupBuzzer(buzzer, 800);
   setupRelay(relay);
 }
 
@@ -32,13 +38,16 @@ void loop()
   if (nowTime - previousTimeGeneral > delayMS)
   {
     previousTimeGeneral = nowTime;
+    handleOta();
     handleDHTSensor();
     handleLightSensor();
     Serial.print("Relay ");
     // Serial.println(!digitalRead(relay));
     triggerRelay();
     handleIr();
-    handleBuzzer(true);
+    // handleBuzzer(true);
+
+    Serial.println(digitalRead(pirPin));
   }
   handleBuzzer(false);
 }
